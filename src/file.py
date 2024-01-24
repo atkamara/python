2aa669f4fed3674c41dc1c317337f22266c318cb
atkamara <155780151+atkamara@users.noreply.github.com>
2024-01-24 08:22:06 +0000

    Create file.py

---
+++ src/file.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+
+import sys
+
+
+def do_something():
+	"""
+
+	This is a simple python function
+
+	"""
+	print("Hello World !!!")
+	print(sys.args)
+
+
+if __name__ == '__main__':
+
+	do_something()
